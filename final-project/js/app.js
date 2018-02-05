9865c1d9333c672a0a9b9b4d8546e021d2874c50
Caryn <caryn.wilson@nytimes.com>
2018-02-05 17:35:46 -0500

    final-project

--- final-project/js/app.js
+++ final-project/js/app.js
@@ -1,9 +1,3 @@
-//Name is centered and when scroll down, name scrolls up
-//after a few scrolls background image changes 3x
-//Third down the page, my name && vision statement changes to Chinese characters; another third it changes to Spanish
-//click on name and website changes all of the big text to Spanish + change background image to me in DR
-//One page layout. When click nav-a items it sends you to different sections of the page
-//
 $('.first-name').hover(function () {
     $('.h1-name').addClass('hover');
     $('.first-name').html('Karina ');
@@ -37,6 +31,6 @@ $('#contact-footer').focus(function (){
 //     'height': '80px'
 //   }, 2000);
 
-$('body').scroll(function () {
-    $('body').html
-})
+// $('body').scroll(function () {
+//     $('body').html
+// })
